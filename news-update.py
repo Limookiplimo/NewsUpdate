51a746de28df0335f9c4043e28f3526f87f63701
Limoo Kiplimo <limoodrice@gmail.com>
2023-05-22 17:05:01 +0300

    scrap with soup

--- news-update.py
+++ news-update.py
@@ -1,42 +1,30 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import requests
+from bs4 import BeautifulSoup
 import psycopg2
 
 # Postgrsql connection
-conn = psycopg2.connect(
-    host="localhost",
-    database="news",
-    user="user",
-    password="user"
-)
-
+conn = psycopg2.connect(host="localhost",database="news",user="user",password="user")
+cur = conn.cursor()
 
 def footballNews():
-    path = "Documents/Projects/News-update/chromedriver_linux64"
-    website = "https://www.thesun.co.uk/sport/football/"
-    service = Service(executable_path=path)
-    driver = webdriver.Chrome(service=service)
-    driver.get(website)
+    URL = "https://www.thesun.co.uk/sport/football/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    items = soup.find_all("div", class_="teaser__copy-container")
 
-    sport_news = driver.find_elements(by='xpath', value='//div [@class="teaser__copy-container"')
-    for item in sport_news:
-        headline = item.find_element(by='xpath',value='./a/h3').text
-        deck = item.find_element(by='xpath',value='./a/p').text
-        link = item.find_element(by='xpath',value='./a').get_attribute('href')
-        
-        cur = conn.cursor()
+    for item in items:
+        headline = item.find("h3", class_="teaser__headline").text.strip()
+        deck = item.find("p", class_="teaser__subdeck").text.strip()
+        link = item.find("a", class_="text-anchor-wrap")['href']
+
         cur.execute("""
                 CREATE TABLE IF NOT EXISTS Football (
                     Headline VARCHAR(255),
                     Deck VARCHAR(255),
-                    Link VARCHAR(255),
+                    Link VARCHAR(255)
                 );
             """)
-        cur.execute("INSERT INTO Football(Headline,Deck,Link,Region) VALUES(%s,%s,%s)",
-            (headline,deck,link)
-        )
+        cur.execute("INSERT INTO Football (headline, deck, link) VALUES (%s, %s, %s)", (headline, deck, link))
         conn.commit()
-        cur.close()
 
-conn.close()
 
