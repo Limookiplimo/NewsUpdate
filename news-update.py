09ede581a2623bac0ed8e47b70c53b577feaeb8a
Limoo Kiplimo <limoodrice@gmail.com>
2023-05-23 15:34:58 +0300

    scrap football, startups & politcs news

--- news-update.py
+++ news-update.py
@@ -7,15 +7,15 @@ conn = psycopg2.connect(host="localhost",database="news",user="user",password="u
 cur = conn.cursor()
 
 def footballNews():
-    URL = "https://www.thesun.co.uk/sport/football/"
-    page = requests.get(URL)
+    website = "https://www.thesun.co.uk/sport/football/"
+    page = requests.get(website)
     soup = BeautifulSoup(page.content, "html.parser")
-    items = soup.find_all("div", class_="teaser__copy-container")
+    elements = soup.find_all("div", class_="teaser__copy-container")
 
-    for item in items:
-        headline = item.find("h3", class_="teaser__headline").text.strip()
-        deck = item.find("p", class_="teaser__subdeck").text.strip()
-        link = item.find("a", class_="text-anchor-wrap")['href']
+    for element in elements:
+        headline = element.find("h3", class_="teaser__headline").text.strip()
+        deck = element.find("p", class_="teaser__subdeck").text.strip()
+        link = element.find("a", class_="text-anchor-wrap")['href']
 
         cur.execute("""
                 CREATE TABLE IF NOT EXISTS Football (
@@ -27,4 +27,54 @@ def footballNews():
         cur.execute("INSERT INTO Football (headline, deck, link) VALUES (%s, %s, %s)", (headline, deck, link))
         conn.commit()
 
+def startupsNews():
+    website = "https://techcrunch.com/category/startups/"
+    page = requests.get(website)
+    soup = BeautifulSoup(page.content, "html.parser")
+    elements = soup.find_all("header", class_="post-block__header")
+
+    for element in elements:
+        title=element.find("h2", class_="post-block__title").text.strip()
+        link=element.find("a", class_="post-block__title__link")["href"]
+        author=element.find("span", class_="river-byline__authors").text.strip()
+        date=element.find("time")["datetime"]
+
+        cur.execute("""
+                CREATE TABLE IF NOT EXISTS Startups (
+                    Title VARCHAR(255),
+                    Link VARCHAR(255),
+                    Author VARCHAR(255),
+                    Date DATE
+                );
+            """)
+        cur.execute("INSERT INTO Startups (title,link,author,date) VALUES(%s,%s,%s,%s)",(title, link, author, date))
+        conn.commit()
+
+def politicsNews():
+    website = "https://www.tuko.co.ke/politics/"
+    page = requests.get(website)
+    soup = BeautifulSoup(page.content, "html.parser")
+    items = soup.find_all("article", class_="c-article-card-no-border")
+
+    for item in items:
+        headline = item.find("a", class_="c-article-card-no-border__headline").text.strip()
+        link = item.find("a")["href"]
+        date = item.find("time")["datetime"]
+
+        cur.execute("""
+                CREATE TABLE IF NOT EXISTS Politics (
+                    Headline VARCHAR(255),
+                    Link VARCHAR(255),
+                    Date DATE
+                );
+            """)
+        cur.execute("INSERT INTO Politics(Headline,Link,Date) VALUES(%s,%s,%s)",(headline, link, date))
+        conn.commit()
+
+footballNews()
+startupsNews()
+politicsNews()
+
+cur.close()
+conn.close()
 
