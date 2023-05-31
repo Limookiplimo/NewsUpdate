bc17aaf0cea09c7514d496255621b9741fd6b654
Limoo Kiplimo <limoodrice@gmail.com>
2023-05-31 14:20:05 +0300

    added DDL dags

--- dags/my_dags.py
+++ dags/my_dags.py
@@ -1,44 +1,31 @@
-from scrape import scrape_sports, scrape_politics, scrape_startups
-from datetime import datetime
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash_operator import BashOperator
+from scrape import scrape_sports, scrape_startups, scrape_politics
 
 default_args = {
-    "start_date":datetime(2023,5,28),
-    "owner":"test_user"
+    'start_date': datetime(2023, 5, 31),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
 }
 
-dag = DAG(
-    dag_id = "my_dag",
-    default_args = default_args,
-    schedule_interval = "*/5 * * * *"
-)
+# DAG for scraping sports data
+with DAG('scrape_sports_dag', default_args=default_args, schedule_interval='@daily') as sports_dag:
+    scrape_sports_task = PythonOperator(
+        task_id='scrape_sports_task',
+        python_callable=scrape_sports
+    )
 
-scrape_sports_task = PythonOperator(
-    task_id = 'scrape_sports',
-    python_callable = scrape_sports,
-    dag = dag,
-)
+# DAG for scraping startup data
+with DAG('scrape_startups_dag', default_args=default_args, schedule_interval='@daily') as startups_dag:
+    scrape_startups_task = PythonOperator(
+        task_id='scrape_startups_task',
+        python_callable=scrape_startups
+    )
 
-scrape_startups_task = PythonOperator(
-    task_id = "scrape_startups",
-    python_callable = scrape_startups,
-    dag = dag,
-)
-
-scrape_politics_task = PythonOperator(
-    task_id = "scrape_politics",
-    python_callable = scrape_politics,
-    dag = dag,
-)
-
-update_task = BashOperator(
-    task_id = 'update',
-    bash_command = "./update.py",
-    dag = dag,
-)
-
-scrape_sports_task >> update_task
-scrape_startups_task >> update_task
-scrape_politics_task >> update_task
+# DAG for scraping politics data
+with DAG('scrape_politics_dag', default_args=default_args, schedule_interval='@daily') as politics_dag:
+    scrape_politics_task = PythonOperator(
+        task_id='scrape_politics_task',
+        python_callable=scrape_politics
+    )
